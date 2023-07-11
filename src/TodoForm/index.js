e28b0611950c10ecb9c95b45f6946e64155cca8d
Daniel Yepes <dyepesc@hotmail.com>
2023-07-10 22:44:23 -0500

    ajuste de placeholder

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -27,7 +27,7 @@ function TodoForm() {
     <form onSubmit={onSubmit}>
       <label>Esribe tu nuevo TODO</label>
       <textarea
-        placeholder="Cortar cebolla para el almuerzo"
+        placeholder="Esribe tu nuevo TODO"
         value={newTodoValue}
         onChange={onChange}
       />
